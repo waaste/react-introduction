e045952635c855ded0252a805fa6d3b8724a6d1d
yann <draganox@gmail.com>
2025-08-31 22:51:41 +0200

    CHANGE: fist hook usage

--- src/script.jsx
+++ src/script.jsx
@@ -1,35 +1,15 @@
-import React from "react";
-import { createRoot } from "react-dom";
-
-const pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h2', {}, props.animal),
-    React.createElement('h3', {}, props.breed),
-  ])
-}
+import { createRoot } from 'react-dom/client'
+import SearchParams from './SearchParams'
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(pet, {
-      animal: 'Dog',
-      name: 'Luna',
-      breed: 'Havanese',
-    }),
-    React.createElement(pet, {
-      animal: 'Bird',
-      name: 'Pepper',
-      breed: 'Cockatiel',
-    }),
-    React.createElement(pet, {
-      animal: 'Cat',
-      name: 'Doink',
-      breed: 'Mixed',
-    }),
-  ])
+  return (
+    <div>
+      <h1>Adopt Me! </h1>
+      <SearchParams />
+    </div>
+  )
 }
 
 const container = document.getElementById('root')
 const root = createRoot(container)
-root.render(React.createElement(App))
+root.render(<App />)
